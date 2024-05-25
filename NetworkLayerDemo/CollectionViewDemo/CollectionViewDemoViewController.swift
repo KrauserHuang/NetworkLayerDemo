58cdf84a1125945be444e5c6c4f615b41e0c9a4c
Krauser Huang <crescentdence@gmail.com>
2024-05-25 15:33:39 +0800

    [Feature] Add UINavigationController connect to ViewController, create CollectionViewDemoViewController

---
+++ NetworkLayerDemo/CollectionViewDemo/CollectionViewDemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CollectionViewDemoViewController.swift
+//  NetworkLayerDemo
+//
+//  Created by Tai Chin Huang on 2024/5/25.
+//
+
+import UIKit
+
+class CollectionViewDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
